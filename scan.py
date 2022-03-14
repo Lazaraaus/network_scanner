4ec1dca38944a19641b6a73f7cadbd60c7103459
Lazaraaus <wood.yjaden@gmail.com>
2022-03-14 02:42:05 -0400

    A-K Done

--- scan.py
+++ scan.py
@@ -4,6 +4,7 @@ import dns.name
 import dns.message
 import dns.query
 import dns.rdatatype
+import dns.reversename
 import sys
 from datetime import datetime, date
 import time
@@ -14,6 +15,7 @@ import os
 import pandas as pd
 SERVER_INFO_KEYS = ['Server', 'server', 'Via', 'via']
 SITE_LIST = []
+TELNET_PORTS = ['80', '443', '22']
 def extract_from_df(df):
 	info = []
 	# Iterate through rows
@@ -22,7 +24,6 @@ def extract_from_df(df):
 		if row['id'] == 'SSLv2':
 			# See if offered
 			offer = row['finding']#.to_string()
-			print(offer)
 			# If so
 			if offer == 'offered (deprecated)' or offer == 'offered':
 				# Append to info list
@@ -106,21 +107,25 @@ def main(filename):
 			answer_ipv4 = resolver.resolve(site, 'A')
 		except:
 			# Set value in dict to None
-			results[site]['ipv4'] = None
+			results[site]['ipv4'] = []
 		else:
+			ipv4s = []
 			for record in answer_ipv4:
 				#record_tuple = (site, record.address)
-				results[site]['ipv4'] = record.address
+				ipv4s.append(record.address)
+			results[site]['ipv4'] = ipv4s
 		try:
 			# Try to resolve for IPV6
 			answer_ipv6 = resolver.resolve(site, 'AAAA')
 		except:
 			# Except
-			results[site]['ipv6'] = None
+			results[site]['ipv6'] = []
 		else:
+			ipv6s = []
 			for record in answer_ipv6:
 				#record_tuple = (site, record.address)#._as_ipv6_address())
-				results[site]['ipv6'] = record.address
+				ipv6s.append(record.address)
+			results[site]['ipv6'] = ipv6s
 
 		# Sever Header
 		# Build site name
@@ -214,27 +219,83 @@ def main(filename):
 		info = extract_from_df(certificate_protocol_data)
 		results[site]['tls_versions'] = info
 
+		# ROOT CERTIFICATE AUTHORITY
+		host_name = site.split('.')[0]
+		out_file = 'ca_info/ca_info_' + host_name + '.txt'
+		shell_openssl_str = 'echo | openssl s_client -connect ' + site + ':443 >' + out_file
+		os.system(shell_openssl_str)
+		content = []
+		line_count = 0
+		with open(out_file, 'r') as file:
+			for line in file:
+				if line != '---\n':
+					content.append(line)
+				elif line == '---\n' and line_count > 1:
+					break
+				line_count += 1
+			file.close()
+		print(content)
+		root_line = content[-1]
+		root_line_split = root_line.split(',')
+		for info_line in root_line_split:
+			if 'O = ' in info_line:
+				info = " ".join(info_line.split(" ")[3:])
+				# Handle Weirdness with Baltimore Cybertrust Root CA
+				if info == 'Baltimore':
+					info = 'Baltimore Cybertrust Root'
+				results[site]['root_ca'] = info
+				break
+		
+		# Reverse DNS Name Lookup
+		reverse_names = []
+		for ipv4 in results[site]['ipv4']:
+			name = dns.reversename.from_address(ipv4)
+			print(name)
+			reverse_names.append(name.to_text())
+		results[site]['rdns_names'] = reverse_names
 
-for site in SITE_LIST:
-		# Print Results entries
-		print(f"Site: {site}")
-		print(f"Site Info: ")
-		pprint.pprint(results[site])
-		print("\n\n")
+		# RTT using Telnet and Subprocess
+		rtt_times = []
+		min_time = 1000000
+		max_time = -100000
+		for ipv4 in results[site]['ipv4']:
+			for telnet_port in TELNET_PORTS:
+				telnet_shell_str = "time echo -e '\x1dclose\x0d' | telnet " +  ipv4 + " 80"
+				init_time = time.time()
+				result = subprocess.run(telnet_shell_str, shell=True, capture_output=True, text=True)
+				if result.returncode == 0:
+					#os.system(telnet_shell_str)
+					end_time = time.time() - init_time
+					if end_time < min_time:
+						min_time = end_time
+					if end_time > max_time:
+						max_time = end_time
+
+					# Port Satisfies telnet call, break
+					break
+		
+		# Geolocations
+		# Add to results dict
+		results[site]['rtt_range'] = [int(round(min_time * 1000)), int(round(max_time * 1000))]
+
+	# Return results dict
+	return results
+def print_info(results):
+	for site in SITE_LIST:
+			# Print Results entries
+			print(f"Site: {site}")
+			print(f"Site Info: ")
+			pprint.pprint(results[site])
+			print("\n\n")
 	
 	
 if __name__ == "__main__":
 	filename = sys.argv[1]
-	# # Sever Header
-	# # Build site name
-	# site_name = 'http://uchicago.edu'
-	# # Build Headers
-	# headers = {'Upgrade-Insecure-Requests': '1'}
-	# # Get headers using head method of requests class
-	# resp = requests.get(site_name, headers=headers)
-	# print(resp.headers)
-	# print(resp.status_code)
-	# print(resp.url)
-	#temp = input("INput something to continue....")
-
-	main(filename)
+	result = subprocess.run("time echo -e \x1dclose\x0d | telnet 142.250.191.174 80", shell=True, capture_output=True, text=True)
+	# result = subprocess.check_output(["nslookup", "northwestern.edu", "8.8.8.8"], timeout=2, stderr=subprocess.STDOUT).decode("utf-8")
+	print(result.args)
+	print(result.returncode)
+	print(result.stdout)
+	#temp = input("Enter a key to continue....")
+	info_dict = main(filename)
+	print_info(info_dict)
